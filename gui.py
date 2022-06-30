985ecf3801f58712415ae7edfa88ccbb690d2b5e
ivanka budimir <xivankax@gmail.com>
2022-06-30 21:32:47 +0200

    datetime_colour

--- gui.py
+++ gui.py
@@ -6,6 +6,8 @@ from security.gui import SecurityModule
 from temperature.gui import TemperatureModule
 from tv.gui import TVModule
 from settings.gui import SettingsModule
+from settings.gui import SettingsColour
+from datetime.gui import DatetimeModule
 
 
 class MySmartHome(tk.Tk):
@@ -60,9 +62,20 @@ class MySmartHome(tk.Tk):
         self.temperature_frame.grid(row=0, column=1)
         self.notebook.add(self.temperature_frame, text="Temperature")
 
-        self.temperature_frame = SettingsModule(self)
-        self.temperature_frame.grid(row=0, column=1)
-        self.notebook.add(self.temperature_frame, text="Settings")
+        self.settings_frame = SettingsModule(self)
+        self.settings_frame.grid(row=0, column=1)
+        self.notebook.add(self.settings_frame, text="Settings")
+
+        self.colour_frame = SettingsColour(self)
+        self.colour_frame.grid(row=0, column=1)
+        self.notebook.add(self.colour_frame, text="Choose colour")
+
+        self.colour_frame = DatetimeModule(self)
+        self.colour_frame.grid(row=0, column=1)
+        
+
+
+
 
 
 root = MySmartHome()
